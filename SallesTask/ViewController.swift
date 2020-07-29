5a1fd7863e9ea7ac6763f8cc7341ff6082f91e80
Pedro <Pedro>
2020-07-29 13:52:14 -0300

    First Commit

---
+++ SallesTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SallesTask
+//
+//  Created by Pedro Oliveira on 7/28/20.
+//  Copyright © 2020 Pedro Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
